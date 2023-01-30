5517c71654ed2064ac2de61c4dfb7fdaab2c019a
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-01-30 14:51:07 +0600

    registration

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require( "express" );
+const mongoose = require( 'mongoose' );
 const app = express();
+const router = express.Router()
 const cors = require( "cors" );
 const { MongoClient, ServerApiVersion, ObjectId } = require( "mongodb" );
 const port = process.env.PORT || 5000;
@@ -8,14 +10,26 @@ require( "dotenv" ).config();
 // middleware
 app.use( cors() );
 app.use( express.json() );
-
+// const usersCollection = require( './Model/UserSchema' )
 
 const uri = `mongodb+srv://${ process.env.DB_user }:${ process.env.DB_password }@cluster0.mdoqsyi.mongodb.net/?retryWrites=true&w=majority`;
 // console.log( uri );
 const client = new MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } );
 
+// mongoose.connect( uri, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// } ).then( () => {
+//     console.log( 'connected' )
+// } )
+//     .catch( ( error ) =>
+//         console.log( 'not connected' ) );
+
 async function run () {
     const billsCollection = client.db( 'BillCalculation' ).collection( 'bills' )
+    const usersCollection = client.db( 'BillCalculation' ).collection( 'users' )
 
     try {
         // post bills
@@ -32,6 +46,49 @@ async function run () {
             const bills = await cursor.toArray();
             res.send( bills );
         } );
+
+        // update billing
+        app.put( '/api/update-billing/:id', async ( req, res ) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId( id ) };
+            const bill = req.body;
+            const option = { upsert: true };
+            const updatedBill = {
+                $set: {
+                    name: bill.name,
+                    email: bill.email,
+                    phone: bill.phone,
+                    amount: bill.amount
+                }
+            }
+            const result = await billsCollection.updateOne( filter, updatedBill, option )
+            res.send( result )
+        } )
+
+        // delete bills
+        app.delete( '/api/delete-billing/:id', async ( req, res ) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId( id ) }
+            const result = await billsCollection.deleteOne( query );
+            // console.log( result )
+            res.send( result )
+        } )
+
+        // register
+        app.post( '/api/registration', async ( req, res ) => {
+            const { name, email, password } = req.body;
+            const query = { email: email };
+            const user = await usersCollection.findOne( query );
+            if ( user ) {
+                const message = 'Email already Exist'
+                return res.send( { acknowledged: false, message } )
+            }
+            // console.log( user );
+            const result = await usersCollection.insertOne( { name, email, password } );
+            res.json( result );
+            // console.log( result );
+            res.status( 201 ).json( { message: "user registration successfully" } )
+        } )
     }
     finally {
 
@@ -47,4 +104,4 @@ app.get( "/", ( req, res ) => {
 app.listen( port, () => {
     console.log( `Billing server is running on ${ port }` );
 } );
-
+module.exports = router;
